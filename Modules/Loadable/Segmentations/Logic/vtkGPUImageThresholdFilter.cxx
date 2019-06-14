80a0f1e2fb1e581ba96958342aa8943b612201e2
Kyle Sunderland <sunderlandkyl@gmail.com>
2019-06-14 14:41:10 -0400

    ENH: Add missing threshold filter

---
+++ Modules/Loadable/Segmentations/Logic/vtkGPUImageThresholdFilter.cxx
@@ -0,0 +1,90 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkGPUImageThresholdFilter.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "vtkGPUImageThresholdFilter.h"
+#include "vtkObjectFactory.h"
+#include "vtkOpenGLShaderProperty.h"
+
+vtkStandardNewMacro(vtkGPUImageThresholdFilter);
+
+// ----------------------------------------------------------------------------
+vtkGPUImageThresholdFilter::vtkGPUImageThresholdFilter()
+{
+  // Fragment shader adapted from https://www.shadertoy.com/view/XdfGDH
+  this->ShaderProperty->SetFragmentShaderCode(R"(
+//VTK::System::Dec
+varying vec2 tcoordVSOutput;
+uniform float zPos;
+//VTK::AlgTexUniforms::Dec
+//VTK::CustomUniforms::Dec
+//VTK::Output::Dec
+void main()
+{
+// Can't have an oversampling factor that is less than zero.
+gl_FragData[0] = vec4(vec3(0.), 1.0);
+if (oversamplingFactor > 0.0)
+  {
+  float offsetStart = -(oversamplingFactor - 1)/(2 * oversamplingFactor);
+  float stepSize = 1.0/oversamplingFactor;
+  float sum = 0;
+
+  float scaledMin = max(minThreshold / (inputScale0 + inputShift0), -1.0);
+  float scaledMax = max(maxThreshold / (inputScale0 + inputShift0), -1.0);
+
+  // Iterate over 216 offset points.
+  for (int k = 0; k < oversamplingFactor; ++k)
+    {
+    for (int j = 0; j < oversamplingFactor; ++j)
+      {
+      for (int i = 0; i < oversamplingFactor; ++i)
+        {
+
+        // Calculate the current offset.
+        vec3 offset = vec3(
+          (offsetStart + stepSize*i)/(inputSize0.x),
+          (offsetStart + stepSize*j)/(inputSize0.y),
+          (offsetStart + stepSize*k)/(inputSize0.z));
+
+        vec3 offsetTextureCoordinate = vec3(tcoordVSOutput, zPos) + offset;
+
+        // If the value of the interpolated offset pixel is greater than the threshold, then
+        // increment the fractional sum.
+        vec4 referenceSample = texture(inputTex0, offsetTextureCoordinate);
+        if (referenceSample.r >= scaledMin && referenceSample.r <= scaledMax )
+          {
+          ++sum;
+          }
+        }
+      }
+    }
+  // Calculate the fractional value of the pixel.
+  sum = sum - 108;
+  sum = sum / (outputScale + outputShift);
+  gl_FragData[0] = vec4( vec3(sum), 1.0 );
+  }
+}
+)");
+}
+
+// ----------------------------------------------------------------------------
+vtkGPUImageThresholdFilter::~vtkGPUImageThresholdFilter()
+{
+}
+
+// ----------------------------------------------------------------------------
+void vtkGPUImageThresholdFilter::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
---
+++ Modules/Loadable/Segmentations/Logic/vtkGPUImageThresholdFilter.h
@@ -0,0 +1,61 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkGPUImageThresholdFilter.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+/**
+ * @class   vtkGPUImageThresholdFilter
+ * @brief   Help image algorithms use the GPU
+ *
+ * Designed to make it easier to accelerate an image algorithm on the GPU
+*/
+
+#ifndef vtkGPUImageThresholdFilter_h
+#define vtkGPUImageThresholdFilter_h
+
+#include "vtkRenderingOpenGL2Module.h" // For export macro
+#include "vtkGPUAbstractImageFilter.h"
+
+class VTK_SLICER_SEGMENTATIONS_LOGIC_EXPORT vtkGPUImageThresholdFilter : public vtkGPUAbstractImageFilter
+{
+public:
+  static vtkGPUImageThresholdFilter *New();
+  vtkTypeMacro(vtkGPUImageThresholdFilter, vtkGPUAbstractImageFilter);
+  void PrintSelf(ostream& os, vtkIndent indent) VTK_OVERRIDE;
+
+  vtkGetMacro(MaxThreshold, double);
+  vtkSetMacro(MaxThreshold, double);
+
+  vtkGetMacro(MinThreshold, double);
+  vtkSetMacro(MinThreshold, double);
+
+  vtkGetMacro(OversamplingFactor, int);
+  vtkSetMacro(OversamplingFactor, int);
+
+
+protected:
+  vtkGPUImageThresholdFilter();
+  ~vtkGPUImageThresholdFilter() VTK_OVERRIDE;
+
+private:
+  vtkGPUImageThresholdFilter(const vtkGPUImageThresholdFilter&) = delete;
+  void operator=(const vtkGPUImageThresholdFilter&) = delete;
+
+protected:
+  int OversamplingFactor;
+  double MinThreshold;
+  double MaxThreshold;
+};
+
+#endif
+
+// VTK-HeaderTest-Exclude: vtkGPUImageThresholdFilter.h
