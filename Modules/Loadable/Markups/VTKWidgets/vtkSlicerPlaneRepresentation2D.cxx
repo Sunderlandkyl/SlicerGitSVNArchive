647e7c768cca04bc2e8bd763bf26406e304c3104
Kyle Sunderland <sunderlandkyl@gmail.com>
2020-03-03 15:13:23 -0500

    ENH: Add header file acknowledgements

--- Modules/Loadable/Markups/VTKWidgets/vtkSlicerPlaneRepresentation2D.cxx
+++ Modules/Loadable/Markups/VTKWidgets/vtkSlicerPlaneRepresentation2D.cxx
@@ -1,20 +1,22 @@
-/*=========================================================================
+/*==============================================================================
 
- Copyright (c) ProxSim ltd., Kwun Tong, Hong Kong. All Rights Reserved.
+  Copyright (c) Laboratory for Percutaneous Surgery (PerkLab)
+  Queen's University, Kingston, ON, Canada. All Rights Reserved.
 
- See COPYRIGHT.txt
- or http://www.slicer.org/copyright/copyright.txt for details.
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 
- This file was originally developed by Davide Punzo, punzodavide@hotmail.it,
- and development was supported by ProxSim ltd.
+  This file was originally developed by Kyle Sunderland, PerkLab, Queen's University
+  and was supported through CANARIE's Research Software Program, Cancer
+  Care Ontario, OpenAnatomy, and Brigham and Women’s Hospital through NIH grant R01MH112748.
 
-=========================================================================*/
+==============================================================================*/
 
 // VTK includes
 #include <vtkActor2D.h>
